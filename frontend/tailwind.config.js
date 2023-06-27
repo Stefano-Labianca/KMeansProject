60db4b5711d8a55d2ebfaefdf330503c7873590e
Stefano-Labianca <lstefano@protonmail.com>
2023-06-27 22:34:28 +0200

    Correct fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Outfit', 'sans-serif']
+				sans: ["Outfit", "sans-serif"],
 			},
 
 			colors: {
-				transparent: 'transparent',
-				current: 'currentColor',
-				background: '#070707',
-				title: '#F1F1EF',
-				subtitle: '#ADADAD',
-				'chart-bg': '#101010',
-				primary: '#7053FF',
-				secondary: '#409FD4',
-				error: '#EB5757'
-			}
-		}
+				transparent: "transparent",
+				current: "currentColor",
+				background: "#070707",
+				title: "#F1F1EF",
+				subtitle: "#ADADAD",
+				"chart-bg": "#101010",
+				primary: "#7053FF",
+				secondary: "#409FD4",
+				error: "#EB5757",
+			},
+		},
 	},
-	plugins: []
-};
+	plugins: [],
+}
