b940f712f92a279375c97dcdd7868f55a51cca68
Stefano-Labianca <lstefano@protonmail.com>
2023-06-27 21:14:06 +0200

    Added support for custom fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Outfit', 'sans-serif']
+			},
+
 			colors: {
 				transparent: 'transparent',
 				current: 'currentColor',
