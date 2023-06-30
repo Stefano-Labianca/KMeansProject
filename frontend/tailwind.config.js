102835c8286a180adcbe3a66550a9402b987567c
Stefano-Labianca <lstefano@protonmail.com>
2023-06-30 21:06:19 +0200

    Added custom font size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Outfit", "sans-serif"],
-			},
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
 
-			colors: {
-				transparent: "transparent",
-				current: "currentColor",
-				background: "#070707",
-				title: "#F1F1EF",
-				subtitle: "#ADADAD",
-				"chart-bg": "#101010",
-				primary: "#7053FF",
-				secondary: "#409FD4",
-				error: "#EB5757",
-			},
-		},
-	},
-	plugins: [],
+      fontSize: {
+        small: "1rem",
+        medium: "1.5rem",
+        large: "2.5rem",
+      },
+
+      colors: {
+        background: "#070707",
+        title: "#F1F1EF",
+        subtitle: "#ADADAD",
+        "chart-bg": "#101010",
+        primary: "#7053FF",
+        secondary: "#409FD4",
+        error: "#EB5757",
+      },
+    },
+  },
+  plugins: [],
 }
