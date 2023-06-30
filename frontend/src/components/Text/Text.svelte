bff7fba37cfea0d57d7203928be41d177c3d8970
Stefano-Labianca <lstefano@protonmail.com>
2023-06-30 21:05:34 +0200

    Completed Text component

---
+++ frontend/src/components/Text/Text.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import type { TextComponent } from "./text"
+
+  export let text: TextComponent["text"]
+  export let role: TextComponent["role"]
+  export let design: TextComponent["design"] = "default"
+</script>
+
+<div class="{role} {design}">
+  {text}
+</div>
+
+<style lang="postcss">
+  /* Roles */
+  .title {
+    @apply text-large;
+    @apply font-bold;
+  }
+
+  .paragraph {
+    @apply text-medium;
+    @apply font-normal;
+  }
+
+  .subtitle {
+    @apply font-light;
+  }
+
+  /* Design */
+  .default {
+    @apply text-title;
+  }
+
+  .primary {
+    @apply text-primary;
+  }
+
+  .secondary {
+    @apply text-subtitle;
+  }
+
+  .error {
+    @apply text-error;
+    @apply font-medium;
+  }
+</style>
---
+++ frontend/src/components/Text/text.d.ts
@@ -0,0 +1,13 @@
+import type { DesignOptions } from "$lib/types"
+
+/**
+ * Descrive le proprieta' pubbliche del componente "Text".
+ * @param text Contenuto testuale del testo
+ * @param role Ruolo del componente. Questo puo' essere un paragrafo, un titolo o un sottotitolo
+ * @param design Tipologia di design da applicare
+ */
+export type TextComponent = {
+  text: string
+  role: "paragraph" | "title" | "subtitle"
+  design?: DesignOptions
+}
