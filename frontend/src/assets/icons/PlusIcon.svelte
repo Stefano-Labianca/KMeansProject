77b24530041d8d69fbb703e2067818b4b8e5fe1d
Stefano-Labianca <lstefano@protonmail.com>
2023-06-30 23:05:52 +0200

    Added icons

---
+++ frontend/src/assets/icons/PlusIcon.svelte
@@ -0,0 +1,6 @@
+<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M16.6287 11.3334H11.6287V16.3334H9.96204V11.3334H4.96204V9.66669H9.96204V4.66669H11.6287V9.66669H16.6287V11.3334Z"
+    fill="currentColor"
+  />
+</svg>
