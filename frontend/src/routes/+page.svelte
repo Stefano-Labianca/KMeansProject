e5bb799f8f1206b612d44ec8e0b5f53e96a8b4e9
Stefano-Labianca <lstefano@protonmail.com>
2023-06-30 21:12:01 +0200

    Added Text tests

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-4xl">Welcome to SvelteKit</h1>
+<script>
+  import Text from "../components/Text/Text.svelte"
+</script>
+
+<Text text="Sono" role="title" design="default" />
+<Text text="Io" role="paragraph" design="primary" />
+<Text text="Marco" role="subtitle" design="secondary" />
+<Text text="Merrino" role="subtitle" design="primary" />
