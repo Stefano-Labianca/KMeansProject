c18fb9db36c589009a8130fc1665979158c59e3c
Stefano Labianca <lstefano@proton.me>
2023-07-05 17:07:51 +0200

    Created Spinner component

---
+++ frontend/src/components/Spinner/Spinner.svelte
@@ -0,0 +1,13 @@
+<div class="Spinner" />
+
+<style lang="postcss">
+  .Spinner {
+    @apply w-20;
+    @apply h-20;
+    @apply border-4;
+    @apply rounded-full;
+    @apply border-t-transparent;
+    @apply border-r-transparent;
+    @apply animate-spin;
+  }
+</style>
