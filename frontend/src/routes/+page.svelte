c5436c3fb9532d67200c9bc40de2237776359c9b
Stefano Labianca <lstefano@proton.me>
2023-07-05 21:30:05 +0200

    Insert visualizations for the buttons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-  import Loading from "../components/Loading/Loading.svelte"
+  import PlusIcon from "../assets/icons/PlusIcon.svelte"
+  import Button from "../components/Button/Button.svelte"
+  import Text from "../components/Text/Text.svelte"
 </script>
 
-<Loading />
+<Text text="k-means project" role="paragraph" />
+
+<div class="flex justify-evenly my-32">
+  <Button text="Add" icon={PlusIcon} design="default" inverted />
+  <Button text="Add" icon={PlusIcon} design="primary" inverted />
+  <Button text="Add" icon={PlusIcon} design="secondary" inverted />
+  <Button text="Add" icon={PlusIcon} design="error" inverted />
+</div>
+
+<div class="flex justify-evenly">
+  <Button text="Add" icon={PlusIcon} design="default" inverted fill />
+  <Button text="Add" icon={PlusIcon} design="primary" inverted fill />
+  <Button text="Add" icon={PlusIcon} design="secondary" inverted fill />
+  <Button text="Add" icon={PlusIcon} design="error" inverted fill />
+</div>
