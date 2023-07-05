49756ddd3f380b9a7a314375767aded6376e943b
Stefano Labianca <lstefano@proton.me>
2023-07-05 20:20:11 +0200

    Added a wrapper type for a generic Svelte component

--- frontend/src/lib/types.d.ts
+++ frontend/src/lib/types.d.ts
@@ -2,3 +2,8 @@
  * Rappresenta i possibili design da applicare ad un componente.
  */
 export type DesignOptions = "default" | "primary" | "secondary" | "error"
+
+/**
+ * Rappresenta un componente Svelte
+ */
+export type BaseComponent = ComponentType<SvelteComponentTyped>
