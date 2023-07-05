74a6ea95fb3e6709b20aba298e7dd41296949d60
Stefano Labianca <lstefano@proton.me>
2023-07-05 17:08:02 +0200

    Created Loading component

---
+++ frontend/src/components/Loading/Loading.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Spinner from "../Spinner/Spinner.svelte"
+</script>
+
+<div class="Loading">
+  <Spinner />
+</div>
+
+<style lang="postcss">
+  .Loading {
+    @apply py-0.5;
+  }
+</style>
