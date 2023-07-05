6929ecd329ae9c8c22fbce6f6d82c91305c440d2
Stefano Labianca <lstefano@proton.me>
2023-07-05 17:08:10 +0200

    Added some tests

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
-  import PlusIcon from "../assets/icons/PlusIcon.svelte"
-  import SearchIcon from "../assets/icons/SearchIcon.svelte"
-  import TrashIcon from "../assets/icons/TrashIcon.svelte"
-  import Icon from "../components/Icon/Icon.svelte"
+  import Loading from "../components/Loading/Loading.svelte"
 </script>
 
-<Icon icon={PlusIcon} design="default" />
-<Icon icon={SearchIcon} design="primary" />
-<Icon icon={TrashIcon} design="secondary" />
-<Icon icon={TrashIcon} design="error" />
+<Loading />
