cc560d958e74138c0c097d9a5a3c572df0374619
Stefano Labianca <lstefano@proton.me>
2023-07-06 11:25:28 +0200

    Added only icons visualizations

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import PlusIcon from "../assets/icons/PlusIcon.svelte"
+  import SearchIcon from "../assets/icons/SearchIcon.svelte"
+  import TrashIcon from "../assets/icons/TrashIcon.svelte"
   import Button from "../components/Button/Button.svelte"
   import Text from "../components/Text/Text.svelte"
 </script>
@@ -13,9 +15,16 @@
   <Button text="Add" icon={PlusIcon} design="error" inverted />
 </div>
 
-<div class="flex justify-evenly">
+<div class="flex justify-evenly my-32">
   <Button text="Add" icon={PlusIcon} design="default" inverted fill />
   <Button text="Add" icon={PlusIcon} design="primary" inverted fill />
   <Button text="Add" icon={PlusIcon} design="secondary" inverted fill />
   <Button text="Add" icon={PlusIcon} design="error" inverted fill />
+</div>
+
+<div class="flex justify-evenly my-32">
+  <Button icon={PlusIcon} design="default" fill />
+  <Button icon={TrashIcon} design="primary" fill />
+  <Button icon={SearchIcon} design="secondary" fill />
+  <Button icon={TrashIcon} design="error" fill />
 </div>
