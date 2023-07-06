2f043fcffc7ca24ebdcdaf2e574151404059bb30
Stefano Labianca <lstefano@proton.me>
2023-07-06 17:15:02 +0200

    Added designs for Spinner

--- frontend/src/components/Spinner/Spinner.svelte
+++ frontend/src/components/Spinner/Spinner.svelte
@@ -1,4 +1,12 @@
-<div class="Spinner" />
+<script lang="ts">
+  import { get } from "svelte/store"
+  import { isButtonFilled, spinnerModifier } from "../../stores/spinner"
+
+  $: designClass = get(spinnerModifier)
+  $: filledButton = get(isButtonFilled)
+</script>
+
+<div class="Spinner {designClass}" class:fill={filledButton} />
 
 <style lang="postcss">
   .Spinner {
@@ -9,5 +17,37 @@
     @apply border-t-transparent;
     @apply border-r-transparent;
     @apply animate-spin;
+  }
+
+  /* Design spinner */
+  .default {
+    @apply border-b-title;
+    @apply border-l-title;
+  }
+
+  .primary {
+    @apply border-b-primary;
+    @apply border-l-primary;
+  }
+
+  .secondary {
+    @apply border-b-subtitle;
+    @apply border-l-subtitle;
+  }
+
+  .error {
+    @apply border-b-error;
+    @apply border-l-error;
+  }
+
+  /* Filled */
+  .fill {
+    @apply border-b-background;
+    @apply border-l-background;
+
+    &.error {
+      @apply border-b-title;
+      @apply border-l-title;
+    }
   }
 </style>
