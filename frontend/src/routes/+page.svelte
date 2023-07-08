763bc7adfb79d625cad5f25e0abe2604e5107ba1
Stefano Labianca <lstefano@proton.me>
2023-07-08 12:23:16 +0200

    Created tests

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,44 +1,34 @@
 <script>
-  import PlusIcon from "../assets/icons/PlusIcon.svelte"
-  import SearchIcon from "../assets/icons/SearchIcon.svelte"
-  import TrashIcon from "../assets/icons/TrashIcon.svelte"
-  import Button from "../components/Button/Button.svelte"
-  import Text from "../components/Text/Text.svelte"
+  import Button from "$components/Button/Button.svelte"
+  import Text from "$components/Text/Text.svelte"
+  import ErrorIcon from "$icons/ErrorIcon.svelte"
+  import LikeIcon from "$icons/LikeIcon.svelte"
+
+  import Alerts from "$layouts/Alerts/Alerts/Alerts.svelte"
+  import alerts from "$stores/alert"
 </script>
 
 <Text text="k-means project" role="paragraph" />
+<Alerts />
 
-<div class="flex justify-evenly my-32">
-  <Button text="Add" icon={PlusIcon} design="default" inverted />
-  <Button text="Add" icon={PlusIcon} design="primary" inverted />
-  <Button text="Add" icon={PlusIcon} design="secondary" inverted />
-  <Button text="Add" icon={PlusIcon} design="error" inverted />
-</div>
+<div class="flex justify-center my-32">
+  <Button
+    text="Success"
+    design="primary"
+    inverted
+    fill
+    onClick={() =>
+      alerts.send({ icon: LikeIcon, text: "Success", design: "primary" })}
+  />
 
-<div class="flex justify-evenly my-32">
-  <Button text="Add" icon={PlusIcon} design="default" inverted fill />
-  <Button text="Add" icon={PlusIcon} design="primary" inverted fill />
-  <Button text="Add" icon={PlusIcon} design="secondary" inverted fill />
-  <Button text="Add" icon={PlusIcon} design="error" inverted fill />
-</div>
+  <div class="mr-32" />
 
-<div class="flex justify-evenly my-32">
-  <Button icon={PlusIcon} design="default" fill />
-  <Button icon={TrashIcon} design="primary" fill />
-  <Button icon={SearchIcon} design="secondary" fill />
-  <Button icon={TrashIcon} design="error" fill />
-</div>
-
-<div class="flex justify-evenly my-32">
-  <Button design="default" loading />
-  <Button design="primary" loading />
-  <Button design="secondary" loading />
-  <Button design="error" loading />
-</div>
-
-<div class="flex justify-evenly my-32">
-  <Button design="default" loading fill />
-  <Button design="primary" loading fill />
-  <Button design="secondary" loading fill />
-  <Button design="error" loading fill />
+  <Button
+    text="Error"
+    design="error"
+    inverted
+    fill
+    onClick={() =>
+      alerts.send({ icon: ErrorIcon, text: "Error", design: "error" })}
+  />
 </div>
