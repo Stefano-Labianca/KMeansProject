2fd3740973f367368967b107670f84977de3a674
Stefano Labianca <lstefano@proton.me>
2023-07-08 12:20:18 +0200

    Added new alias

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,14 +3,9 @@ import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       precompress: true,
     }),
@@ -18,6 +13,9 @@ const config = {
       $lib: "./src/lib",
       $pages: "./src/pages",
       $stores: "./src/stores",
+      $components: "./src/components",
+      $icons: "./src/assets/icons",
+      $layouts: "./src/layouts",
     },
   },
 }
