1cf556c59bd27243d8426339241717f98e3381f8
Stefano Labianca <lstefano@proton.me>
2023-07-08 12:23:04 +0200

    Added Alters layout

---
+++ frontend/src/layouts/Alerts/Alerts/Alerts.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Alert from "$components/Alert/Alert.svelte"
+  import {
+    TRANSITION_BASE,
+    TRANSITION_Y_IN,
+    TRANSITION_Y_OUT,
+  } from "$lib/consts"
+  import alerts from "$stores/alert"
+  import { flip } from "svelte/animate"
+  import { fly } from "svelte/transition"
+</script>
+
+<div class="Alerts">
+  {#each $alerts as alert (alert)}
+    <div
+      animate:flip={TRANSITION_BASE}
+      in:fly={TRANSITION_Y_IN}
+      out:fly={TRANSITION_Y_OUT}
+    >
+      <Alert {...alert} />
+    </div>
+  {/each}
+</div>
+
+<style lang="postcss">
+  .Alerts {
+    @apply fixed;
+    @apply top-0;
+    @apply right-0;
+    @apply w-full;
+    @apply p-small;
+    @apply grid;
+    @apply gap-small;
+    @apply z-50;
+
+    @apply md:max-w-lg;
+  }
+</style>
