26f8fe827d05fff58d7e54b0dfd3f38b28e0f763
Stefano Labianca <lstefano@proton.me>
2023-07-10 20:09:23 +0200

    Added Entry component

---
+++ frontend/src/components/Entry/Entry.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Button from "$components/Button/Button.svelte"
+  import Text from "$components/Text/Text.svelte"
+  import TrashIcon from "$icons/TrashIcon.svelte"
+  import type { EntryComponent } from "./entry"
+
+  export let title: EntryComponent["title"]
+  export let date: EntryComponent["date"]
+</script>
+
+<div class="Entry">
+  <div class="entry-content">
+    <Text role="paragraph" text={title} />
+    <Text role="subtitle" text={date} design="secondary" />
+  </div>
+
+  <div class="entry-button">
+    <Button design="error" icon={TrashIcon} />
+  </div>
+</div>
+
+<style lang="postcss">
+  .Entry {
+    @apply flex;
+    @apply justify-between;
+    @apply bg-secondary/light;
+    @apply pl-default;
+    @apply rounded-default;
+  }
+
+  .entry-content {
+    @apply py-small;
+  }
+</style>
