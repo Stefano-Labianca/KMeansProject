f544aa9b198a3a2472cdd6757ba05f8a1e302164
Stefano Labianca <lstefano@proton.me>
2023-07-10 17:50:08 +0200

    Added test visualization

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,34 +1,16 @@
 <script>
-  import Button from "$components/Button/Button.svelte"
+  import Input from "$components/Input/Input.svelte"
   import Text from "$components/Text/Text.svelte"
-  import ErrorIcon from "$icons/ErrorIcon.svelte"
-  import LikeIcon from "$icons/LikeIcon.svelte"
 
   import Alerts from "$layouts/Alerts/Alerts/Alerts.svelte"
-  import alerts from "$stores/alert"
 </script>
 
 <Text text="k-means project" role="paragraph" />
 <Alerts />
 
-<div class="flex justify-center my-32">
-  <Button
-    text="Success"
-    design="primary"
-    inverted
-    fill
-    onClick={() =>
-      alerts.send({ icon: LikeIcon, text: "Success", design: "primary" })}
-  />
-
-  <div class="mr-32" />
-
-  <Button
-    text="Error"
-    design="error"
-    inverted
-    fill
-    onClick={() =>
-      alerts.send({ icon: ErrorIcon, text: "Error", design: "error" })}
-  />
+<div class="w-full flex my-12">
+  <div class="flex items-center gap-medium">
+    <Input type="number" placeholder="Inserire dei numeri" />
+    <Input type="text" placeholder="Scrivi quello che vuoi" />
+  </div>
 </div>
