2e8640a15c0576d3db8b9ab158ff4dc25e43dc67
Stefano Labianca <lstefano@proton.me>
2023-07-13 17:38:47 +0200

    Created History component

---
+++ frontend/src/components/History/History.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Entry from "$components/Entry/Entry.svelte"
+  import type { EntryComponent } from "$components/Entry/entry"
+  import history from "$stores/history"
+  import dayjs from "dayjs"
+  import { nanoid } from "nanoid"
+
+  history.update(() => {
+    return [
+      {
+        id: nanoid(),
+        title: "First",
+        date: dayjs().format("D MMMM YYYY"),
+      },
+      {
+        id: nanoid(),
+        title: "Second",
+        date: dayjs().add(1, "day").format("D MMMM YYYY"),
+      },
+      {
+        id: nanoid(),
+        title: "Third",
+        date: dayjs().add(2, "day").format("D MMMM YYYY"),
+      },
+    ] as EntryComponent[]
+  })
+</script>
+
+<div class="History">
+  {#each $history as entry (entry)}
+    <div class="entry">
+      <Entry {...entry} />
+    </div>
+  {/each}
+</div>
+
+<style lang="postcss">
+  .History {
+  }
+
+  .entry {
+    @apply mt-6;
+  }
+</style>
