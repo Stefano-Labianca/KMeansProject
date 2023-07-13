02cb01ebc36a1c541b05eb10156f7afd6d1d359c
Stefano Labianca <lstefano@proton.me>
2023-07-13 18:42:58 +0200

    Created EmptyState component

---
+++ frontend/src/components/EmptyState/EmptyState.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Gliph from "$components/Gliph/Gliph.svelte"
+  import Text from "$components/Text/Text.svelte"
+  import type { EmptyStateComponent } from "./emptyState"
+
+  export let gliph: EmptyStateComponent["gliph"]
+  export let text: EmptyStateComponent["text"]
+  export let subtitle: EmptyStateComponent["subtitle"]
+</script>
+
+<div class="EmptyState">
+  <Gliph {gliph} />
+  <Text role="title" {text} />
+  <Text role="paragraph" design="secondary" text={subtitle} />
+</div>
+
+<style lang="postcss">
+  .EmptyState {
+    @apply flex;
+    @apply items-center;
+    @apply justify-center;
+    @apply flex-col;
+  }
+</style>
