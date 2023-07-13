20669bbfa4ab0c077fd401c1f67c8ad1a878ff52
Stefano Labianca <lstefano@proton.me>
2023-07-13 18:25:54 +0200

    Created Gliph component

---
+++ frontend/src/components/Gliph/Gliph.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { GliphComponent } from "./gliph"
+
+  export let gliph: GliphComponent["gliph"]
+</script>
+
+<div class="Gliph">
+  <svelte:component this={gliph} />
+</div>
+
+<style lang="postcss">
+  .Gliph {
+    @apply w-gliph;
+    @apply h-gliph;
+  }
+</style>
