4c72c7be77169ac82180d2405704c64698822314
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-08-13 11:27:07 +0200

    task 1 frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,11 +2,15 @@
   import Button from "$components/Button/Button.svelte"
   import Text from "$components/Text/Text.svelte"
   import Alerts from "$layouts/Alerts/Alerts/Alerts.svelte"
+  import KMeansEndPoint from "../api/kmeans"
   import type { KMeans } from "../types/kmeans"
 
-  let kmeansRes: KMeans | undefined
+  // let kmeansRes: KMeans | undefined
 
-  const caller = async () => {}
+  const caller = async () => {
+    let kmeansRes = await KMeansEndPoint.calculate<KMeans>("/api/calculate", "playtennis", 4)
+    console.log(kmeansRes)
+  }
 </script>
 
 <Text text="k-means project" role="paragraph" />
