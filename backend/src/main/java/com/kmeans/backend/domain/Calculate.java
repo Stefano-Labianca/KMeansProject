643f138967467c0118c63c95ccfeff7cd7d496b2
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-08-13 10:57:50 +0200

    Task 2

---
+++ backend/src/main/java/com/kmeans/backend/domain/Calculate.java
@@ -0,0 +1,11 @@
+package com.kmeans.backend.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+public class Calculate {
+  private String tableName;
+  private Integer k;
+
+}
