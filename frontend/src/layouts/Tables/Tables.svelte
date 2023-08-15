39b643b48dfa97fbafd95421f3968c8b17e3515e
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-08-15 14:21:33 +0200

    Task 3

---
+++ frontend/src/layouts/Tables/Tables.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Table from "$components/Table/Table.svelte"
+  import Text from "$components/Text/Text.svelte"
+  import { beforeUpdate } from "svelte"
+  import type { Cluster, Example, Middle } from "../../types/kmeans"
+  import type { TablesComponent } from "./tables"
+
+  export let tables: TablesComponent["tables"]
+
+  let middlesColumns: string[]
+  let exampleColumns: string[]
+  let middles: Middle[]
+
+  let examples: Example[][]
+  let avgDistances: number[]
+</script>
