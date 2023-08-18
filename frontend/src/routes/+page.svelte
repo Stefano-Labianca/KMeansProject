e7864e073720c071ef4cf72a1f93997ccb022840
Stefano Labianca <lstefano@proton.me>
2023-08-18 19:24:27 +0200

    Useful test

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
   import Button from "$components/Button/Button.svelte"
+  import type { EntryComponent } from "$components/Entry/entry"
+  import History from "$components/History/History.svelte"
   import Text from "$components/Text/Text.svelte"
   import Alerts from "$layouts/Alerts/Alerts/Alerts.svelte"
+  import dayjs from "dayjs"
+  import CrudEndPoint from "../api/crud"
   import KMeansEndPoint from "../api/kmeans"
-  import type { KMeans } from "../types/kmeans"
+  import { DAYJS_FORMAT } from "../const"
+  import type { HistoryEntry, KMeans } from "../types/kmeans"
 
-  let kmeansRes: KMeans | undefined
+  let history = [] as EntryComponent[]
+  let kMeans: KMeans | undefined
 
-  const notFound = async () => {
-    await KMeansEndPoint.calculate<KMeans>("/api/calculate", "aaaa", 5)
+  const create = async () => {
+    kMeans = await KMeansEndPoint.calculate<KMeans>("/api/calculate", "playtennis", 2)
+    console.log(kMeans)
   }
 
-  /** Spegni Docker per farlo funzionare */
-  const databaseError = async () => {
-    await KMeansEndPoint.calculate<KMeans>("/api/calculate", "playtennis", 5)
+  const save = async () => {
+    if (kMeans) {
+      let payload = {
+        ...kMeans,
+        date: dayjs().format(DAYJS_FORMAT),
+        name: "Dio merda",
+      }
+      let response = await CrudEndPoint.create<KMeans>("/history/add", payload)
+      console.log("response: ", response)
+    }
   }
 
-  const negativeK = async () => {
-    await KMeansEndPoint.calculate<KMeans>("/api/calculate", "playtennis", -5)
-  }
-
-  const bigFatK = async () => {
-    await KMeansEndPoint.calculate<KMeans>("/api/calculate", "playtennis", 10000)
+  const findAll = async () => {
+    let res = await CrudEndPoint.read<HistoryEntry>("/history/get")
+    console.log(res)
   }
 </script>
 
@@ -30,13 +41,13 @@
 
 <div class="mt-20" />
 
-<Button text="Table not found error" onClick={notFound} fill />
-<div class="mt-4" />
+<Button design="primary" fill text="Get KMeans" onClick={create} />
+<div class="mt-20" />
 
-<Button text="Database connection error" onClick={databaseError} fill />
-<div class="mt-4" />
+<Button design="primary" fill text="Save KMeans" onClick={save} />
+<div class="mt-20" />
 
-<Button text="k is too big" onClick={bigFatK} fill />
-<div class="mt-4" />
+<Button design="primary" fill text="Find All" onClick={findAll} />
+<div class="mt-20" />
 
-<Button text="k is negative" onClick={negativeK} fill />
+<History historyData={history} />
