a8da45c871d3b36834f678ea870ef5ebdf9bcd73
Stefano Labianca <lstefano@proton.me>
2023-08-18 19:24:21 +0200

    Created a mapping for the database and Java class

--- backend/src/main/java/com/kmeans/converter/APIRequest.java
+++ backend/src/main/java/com/kmeans/converter/APIRequest.java
@@ -2,10 +2,23 @@ package com.kmeans.converter;
 
 import java.util.ArrayList;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@Document(collection = "calculation")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class APIRequest {
+
+    @Id
+    private String id;
+
+    private String date;
+    private String name;
     private ArrayList<String> columnsName = new ArrayList<String>();
     private ArrayList<Object> clusters = new ArrayList<>();
     private Integer iteration;
