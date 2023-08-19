6128cca4b6c3599c3f528ff768e4edf95cfc4e51
Stefano Labianca <lstefano@proton.me>
2023-08-19 15:30:26 +0200

    ✨BURRO ANIMATION✨

--- frontend/src/components/History/History.svelte
+++ frontend/src/components/History/History.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
   import Entry from "$components/Entry/Entry.svelte"
-  import { TRANSITION_BASE, TRANSITION_X_OUT, TRANSITION_Y_IN } from "$lib/consts"
+  import { DELAY, TRANSITION_BASE, TRANSITION_X_OUT, TRANSITION_Y_IN } from "$lib/consts"
 
   import history from "$stores/history"
-
   import { flip } from "svelte/animate"
   import { fly } from "svelte/transition"
   import type { HistoryComponent } from "./history"
 
   export let historyData: HistoryComponent["history"] = undefined
-
-  if (historyData) history.addEntries(historyData)
 </script>
 
 {#if historyData}
   <div class="History">
-    {#each $history as entry (entry)}
-      <div class="entry" animate:flip={TRANSITION_BASE} in:fly={TRANSITION_Y_IN} out:fly={TRANSITION_X_OUT}>
+    {#each $history as entry, i (entry)}
+      <div
+        class="entry"
+        animate:flip={TRANSITION_BASE}
+        in:fly={{ ...TRANSITION_Y_IN, delay: i * DELAY }}
+        out:fly={TRANSITION_X_OUT}
+      >
         <Entry {...entry} />
       </div>
     {/each}
