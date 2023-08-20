ad365b3fe7db8da216e0aa05f7f77809975c4043
Stefano Labianca <lstefano@proton.me>
2023-08-20 20:20:37 +0200

    added missing import

--- backend/src/main/java/com/kmeans/backend/controller/HistoryController.java
+++ backend/src/main/java/com/kmeans/backend/controller/HistoryController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.kmeans.backend.repository.HistoryRepository;
 import com.kmeans.converter.APIRequest;
