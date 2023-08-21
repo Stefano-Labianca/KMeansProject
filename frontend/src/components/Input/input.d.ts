6bed0c5817d35c16e59a03a9abfe1d4e6654aed6
Stefano Labianca <lstefano@proton.me>
2023-08-21 16:58:25 +0200

    Added error component

--- frontend/src/components/Input/input.d.ts
+++ frontend/src/components/Input/input.d.ts
@@ -1,11 +1,11 @@
-import type { IComponent } from "$lib/types"
 import type { HTMLInputAttributes } from "svelte/elements"
 
-export interface IInput extends HTMLInputAttributes {
+export interface InputComponent extends HTMLInputAttributes {
   label?: string
-  icon?: IComponent
+  icon?: BaseComponent
   placeholder?: string
   id?: string
   optional?: boolean
   value?: string | number
+  error?: string
 }
