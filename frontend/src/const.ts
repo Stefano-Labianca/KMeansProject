7e6e1225f1b667bea4ae8a7226fc2a449d3353d3
Stefano Labianca <lstefano@proton.me>
2023-08-21 13:15:59 +0200

    created endpoint constant

--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,3 +1,11 @@
 export const BASE_URL = "http://localhost:8080"
 
 export const DAYJS_FORMAT = "D MMMM YYYY"
+
+export const HISTORY_ENDPOINT = {
+  GET: "/history/get",
+  POST: "/history/add",
+  DELETE: "/history/delete",
+}
+
+export const API_CALCULATE = "/api/calculate"
