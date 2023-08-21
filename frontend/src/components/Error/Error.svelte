fde3bb69be5b7000aab31a46c42e9abde40408b9
Stefano Labianca <lstefano@proton.me>
2023-08-21 16:58:20 +0200

    Created error component

---
+++ frontend/src/components/Error/Error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { TRANSITION_BASE } from "$lib/consts"
+  import { slide } from "svelte/transition"
+  import Text from "../Text/Text.svelte"
+  import type { ErrorComponent } from "./error"
+
+  export let error: ErrorComponent["error"] = ""
+</script>
+
+{#if error}
+  <div class="Error" transition:slide={TRANSITION_BASE}>
+    <Text text={error} design="error" role="subtitle" />
+  </div>
+{/if}
+
+<style lang="postcss">
+  .Error {
+    @apply mt-[0.5rem];
+  }
+</style>
