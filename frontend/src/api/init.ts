919dea3c7f4e76997e2605f34951ec3f7dbaba26
Stefano Labianca <lstefano@proton.me>
2023-10-13 12:21:53 +0200

    Send error alert when the server is not online

--- frontend/src/api/init.ts
+++ frontend/src/api/init.ts
@@ -1,3 +1,4 @@
+import type { EntryComponent } from "$components/Entry/entry"
 import dayjs from "dayjs"
 import { nanoid } from "nanoid"
 import { DAYJS_FORMAT, HISTORY_ENDPOINT } from "../const"
@@ -5,14 +6,17 @@ import type { HistoryEntry, KMeans } from "../types/kmeans"
 import CrudEndPoint from "./crud"
 
 export const findAll = async () => {
-  const res = await CrudEndPoint.read<HistoryEntry>(HISTORY_ENDPOINT.GET)
+  try {
+    const res: HistoryEntry[] = await CrudEndPoint.read<HistoryEntry>(HISTORY_ENDPOINT.GET)
+    const historyData: EntryComponent[] = res.map(e => {
+      const { date, title, id } = e
+      return { date, title, id }
+    })
 
-  const historyData = res.map(e => {
-    const { date, title, id } = e
-    return { date, title, id }
-  })
-
-  return historyData
+    return historyData
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export const save = async (record: KMeans) => {
@@ -24,5 +28,11 @@ export const save = async (record: KMeans) => {
     title: nanoid(10),
   } as HistoryEntry
 
-  await CrudEndPoint.create<HistoryEntry>(HISTORY_ENDPOINT.POST, payload)
+  try {
+    // TODO: Spezza il cristo di playload in caso di cluster enormi
+
+    await CrudEndPoint.create<HistoryEntry>(HISTORY_ENDPOINT.POST, payload)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
