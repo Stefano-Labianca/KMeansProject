39d520662ae0e3ef61ee705ad45022168e375d22
Stefano Labianca <lstefano@proton.me>
2023-10-13 12:24:01 +0200

    Updated ts files

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,8 +1,24 @@
+import type { AlertComponent } from "$components/Alert/alert"
+import type { EntryComponent } from "$components/Entry/entry"
+import ErrorIcon from "$icons/ErrorIcon.svelte"
+import alert from "$stores/alert"
 import { findAll } from "../api/init"
 import type { PageLoad } from "./$types"
 
 export const load: PageLoad = async () => {
+  let historyData: EntryComponent[] = []
+
+  try {
+    historyData = await findAll()
+  } catch (error) {
+    alert.send({
+      text: "Server connection error",
+      icon: ErrorIcon,
+      design: "error",
+    } as AlertComponent)
+  }
+
   return {
-    historyData: await findAll(),
+    historyData,
   }
 }
--- frontend/src/types/crudTypes.d.ts
+++ frontend/src/types/crudTypes.d.ts
@@ -4,4 +4,4 @@ type ReadOne = <T>(url: string, id: string) => Promise<T>
 
 type Create = <T>(url: string, payload: T) => Promise<void>
 
-type Delete = (url: string, id: string) => Promise<void>
+type Delete = (url: string, id: string) => Promise<unknown>
